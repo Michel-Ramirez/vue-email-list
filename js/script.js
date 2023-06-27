1805c241c3c6b002858cc7a7b04ff6c8eb4d7d02
Michel-Ramirez <michel.ramirez19@gmail.com>
2023-06-27 15:35:11 +0200

    fix mounted

--- js/script.js
+++ js/script.js
@@ -8,22 +8,16 @@ const app = Vue.createApp({
             items : 10,
         }
     },
-    methods: {
-        getEmail() {
-            
-            for (let i = 0; i < items; i++){
-                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-                .then(res => {
-                    const mail = res.data.response;
-                    this.mails.push(mail);
-                })
-            }
-
-        },
+    mounted(){
+        
+        for (let i = 0; i < this.items; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(res => {
+                const mail = res.data.response;
+                this.mails.push(mail);
+            });
+        };
     },
-    // created(){
-    //     getEmail();
-    // },
 });
 
 
